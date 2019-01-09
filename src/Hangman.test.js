9abeebfc6d4a98312ff17681b40f6882260a3d9a
Chad Schroeder <gmchad@gmail.com>
2019-01-09 12:39:03 -0800

    updated initial tests to use .find

--- src/Hangman.test.js
+++ src/Hangman.test.js
@@ -19,9 +19,8 @@ describe('<Hangman>', () => {
     wrapper
       .find('button[value="b"]')
       .simulate('click', { target: { value: 'b' } });
-    const serialzed = toJson(wrapper);
-    const imgSrc = serialzed.children[0].props;
-    expect(imgSrc).toEqual({ src: '1.jpg' });
+    const imgProps = wrapper.find('img').props();
+    expect(imgProps).toEqual({ src: '1.jpg' });
   });
 
   // check if it is possible to check for expect equality within serialized itself.
@@ -30,11 +29,9 @@ describe('<Hangman>', () => {
     wrapper
       .find('button[value="a"]')
       .simulate('click', { target: { value: 'a' } });
-    const serialized = toJson(wrapper);
-    const imgSrc = serialized.children[0].props;
-    console.log(serialized.children[1].children);
-    const letters = serialized.children[1].children;
-    expect(imgSrc).toEqual({ src: '0.jpg' });
+    const imgProps = wrapper.find('img').props();
+    const letters = wrapper.find('.Hangman-word').props().children;
+    expect(imgProps).toEqual({ src: '0.jpg' });
     expect(letters).toContain('a');
   });
 });
