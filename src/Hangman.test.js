b837b18d1fa47b40410197724336271b44f13e50
P Virk <virk@outlook.com>
2019-01-09 12:25:30 -0800

    added for letter check

--- src/Hangman.test.js
+++ src/Hangman.test.js
@@ -23,4 +23,18 @@ describe('<Hangman>', () => {
     const imgSrc = serialzed.children[0].props;
     expect(imgSrc).toEqual({ src: '1.jpg' });
   });
+
+  // check if it is possible to check for expect equality within serialized itself.
+  it('should change revealed letters but keep the same image with correct guess of "a"', () => {
+    const wrapper = shallow(<Hangman />);
+    wrapper
+      .find('button[value="a"]')
+      .simulate('click', { target: { value: 'a' } });
+    const serialized = toJson(wrapper);
+    const imgSrc = serialized.children[0].props;
+    console.log(serialized.children[1].children);
+    const letters = serialized.children[1].children;
+    expect(imgSrc).toEqual({ src: '0.jpg' });
+    expect(letters).toContain('a');
+  });
 });
